8bce9411559607611227ae318f28be3e2d606a5d
Fernanda Lima <75097486+femilkbr@users.noreply.github.com>
2022-02-24 18:23:02 -0300

    Retirando as minhas credenciais do arquivo

--- twitterClient.js
+++ twitterClient.js
@@ -1,10 +1,12 @@
 const {TwitterApi} = require("twitter-api-v2");
 
+//Pegue suas credenciais depois de realizar o cadastro no Twitter Develop
+
 const client = new TwitterApi({
-    appKey: "XXeUSPywKt2MmvVzhA0k1PvXO",
-    appSecret: "BJMDYPk3hhgTGhbZp1MpJcKx5qpxeCbWgdAutuIpGkSZ6L5vTK",
-    accessToken: "1262145293270867971-F7QGZHgxVgsk6cQ1TPo7QNMArDBQiB",
-    accessSecret: "X8uCfX8QFyaM7W2RmmZBTUGeNPe65w8UePNrlxVxipr4F"
+    appKey: "xxx",
+    appSecret: "xxx",
+    accessToken: "xxx",
+    accessSecret: "xxx"
 })
 
 const rwClient = client.readWrite
